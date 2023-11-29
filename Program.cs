2a3070343f588e9a83b13af3b43badc81407a35f
hecsolmay <hecsolmay@gmail.com>
2023-11-29 01:55:28 -0600

    add Start from program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿global using facturacion_taller_p3.Models;
+global using facturacion_taller_p3.Service;
+global using facturacion_taller_p3.Interface;
+global using facturacion_taller_p3.View;
+
+
+List<Pieza> piezas = new List<Pieza> {
+  new Pieza {Nombre = "Prueba", NumeroDePieza = 1, Precio = 10, Tiempo = 5}
+};
+List<Kits> kits = new List<Kits>();
+
+Inicio inicio = new Inicio(kits, piezas);
+
+inicio.Mostrar();
